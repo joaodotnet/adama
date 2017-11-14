10b1e2db9ef4d5217b3e3f420d8cabc1011fd75e
João Gonçalves <joao.goncalves@x4dev.pt>
2017-11-14 18:46:50 +0000

    add damasalesapp change locations

---
+++ src/DamaSalesApp/DamaSales/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DamaSales
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Sales_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Pages.Sales());
+        }
+
+        private async void Orders_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Pages.Orders());
+        }
+    }
+}
