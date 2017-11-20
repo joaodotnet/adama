d3c589a3afa565b30a2cec9e1f02165f7c565afc
João Gonçalves <joao.goncalves@x4dev.pt>
2017-11-20 18:43:20 +0000

    small changes

--- src/DamaSalesApp/DamaSales/Pages/Sales.xaml.cs
+++ src/DamaSalesApp/DamaSales/Pages/Sales.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using DamaSales.Models;
+using DamaSales.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -10,14 +11,14 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace DamaSales.Pages
-{    
+{
     public partial class Sales : ContentPage
     {
         public ObservableCollection<string> Items { get; set; }
 
         public Sales()
         {
-            InitializeComponent();            
+            InitializeComponent();
         }
 
         protected override void OnAppearing()
@@ -32,22 +33,78 @@ namespace DamaSales.Pages
                 new Category { Id = 5, Name = "Personalizado" }
             };
 
-            this.BindingContext = Items;
+            //var listViewModel = new List<CategoryViewModel>();
+            //for (int i = 0; i < Items.Count; i = i + 4)
+            //{
+            //    var items = Items.Skip(i).Take(4).ToList();
+            //    listViewModel.Add(new CategoryViewModel
+            //    {
+            //        Category1Name = items[0].Name,
+            //        HasCategory2 = items.Count > 1,
+            //        Category2Name = items.Count > 1 ? items[1].Name : string.Empty,
+            //        HasCategory3 = items.Count > 2,
+            //        Category3Name = items.Count > 2 ? items[2].Name : string.Empty,
+            //        HasCategory4 = items.Count > 3,
+            //        Category4Name = items.Count > 3 ? items[3].Name : string.Empty,
+            //    });
+            //}
+
+            //this.BindingContext = listViewModel;
+
+            var model = new CategoryViewModel
+            {
+                CurrentDate = DateTime.Now.ToString("yyyy-MM-dd"),
+                Place = "Feira de Quarteira"
+            };
+
+            int row = 0, column = 0;
+            foreach (var item in Items)
+            {
+                var tapGestureRecognizer = new TapGestureRecognizer();
+                tapGestureRecognizer.Tapped += async (s, e) => {
+                    await DisplayAlert("Item Tapped", $"{item.Name} tapped", "OK");
+                };
+
+                StackLayout stack = new StackLayout();
+                //var image = new Image { Source = "http://via.placeholder.com/100x100" };
+                //image.GestureRecognizers.Add(tapGestureRecognizer);
+                var box = new BoxView { HeightRequest = 100, WidthRequest = 100, Color = Color.LawnGreen};
+                box.GestureRecognizers.Add(tapGestureRecognizer);
+                stack.Children.Add(box);
+                stack.Children.Add(new Label { Text = item.Name, VerticalOptions = LayoutOptions.CenterAndExpand, HorizontalOptions = LayoutOptions.CenterAndExpand });
+                categoriesList.Children.Add(stack, column, row);
+                column++;
+                if (column == 4)
+                {
+                    column = 0;
+                    row++;
+                }
+
+            }
+            //Content = categoriesList;
+
+            //lblDate.Text = DateTime.Now.ToString("yyyy-MM-dd");
+            //lblPlace.Text = "Feira de Quarteira";
+
+            this.BindingContext = model;
 
             //categoriesListView.IsRefreshing = false;
 
             base.OnAppearing();
         }
 
+        
+
         async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
         {
             if (e.Item == null)
                 return;
 
-            await DisplayAlert("Item Tapped", "An item was tapped.", "OK");
+            //await DisplayAlert("Item Tapped", "An item was tapped.", "OK");
 
             //Deselect Item
             ((ListView)sender).SelectedItem = null;
         }
     }
+
 }
---
+++ src/DamaSalesApp/DamaSales/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DamaSales.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public string CurrentDate { get; set; }
+        public string Place { get; set; }
+    }
+}
