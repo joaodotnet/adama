e6576d3c17244636c73805b354bbe072965bee63
João Gonçalves <joaofbbg@gmail.com>
2017-11-23 01:29:34 +0000

    product types page

---
+++ src/DamaSalesApp/DamaSales/ViewModels/ViewType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DamaSales.ViewModels
+{
+    public enum ViewType
+    {
+        CATEGORY,
+        PRODUCT_TYPE
+    }
+}
