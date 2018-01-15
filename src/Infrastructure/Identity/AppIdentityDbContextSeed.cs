678479046e569828b05a23eb2a2acd0764c174f4
João Gonçalves <joaofbbg@gmail.com>
2018-01-15 22:07:12 +0000

    add lib in backoffice

--- src/Infrastructure/Identity/AppIdentityDbContextSeed.cs
+++ src/Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -7,8 +7,11 @@ namespace Infrastructure.Identity
     {
         public static async Task SeedAsync(UserManager<ApplicationUser> userManager)
         {
-            var defaultUser = new ApplicationUser { UserName = "demouser@microsoft.com", Email = "demouser@microsoft.com" };
-            await userManager.CreateAsync(defaultUser, "Pass@word1");
+            var defaultUser = new ApplicationUser { UserName = "joaofbbg@gmail.com", Email = "joaofbbg@gmail.com" };
+            await userManager.CreateAsync(defaultUser, "dama#2017!");
+
+            var damaUser = new ApplicationUser { UserName = "susana.m.mendez@gmail.com", Email = "susana.m.mendez@gmail.com" };
+            await userManager.CreateAsync(damaUser, "damasite#2017!");
         }
     }
 }
