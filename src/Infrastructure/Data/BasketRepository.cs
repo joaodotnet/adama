c9bf4e2d8a7e8321e8056acd3fb9eed247f19a34
João Gonçalves <joao.goncalves@x4dev.pt>
2018-02-27 23:12:20 +0000

    basket repository added

---
+++ src/Infrastructure/Data/BasketRepository.cs
@@ -0,0 +1,32 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Entities.OrderAggregate;
+using ApplicationCore.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class BasketRepository : EfRepository<Basket>, IBasketRepository
+    {
+        public BasketRepository(DamaContext dbContext) : base(dbContext)
+        {
+        }
+
+        public Basket GetByIdWithItems(int id)
+        {
+            return _dbContext.Baskets
+                .Include(b => b.Items)
+                .Include("Items.Details")
+                .FirstOrDefault();
+        }
+
+        public Task<Basket> GetByIdWithItemsAsync(int id)
+        {
+            return _dbContext.Baskets
+                .Include(b => b.Items)
+                .Include("Items.Details")
+                .FirstOrDefaultAsync();
+        }
+    }
+}
