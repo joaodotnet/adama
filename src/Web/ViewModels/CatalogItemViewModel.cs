c8f0ccbbcc4d568fc2fe642b99f39673c508c891
João Gonçalves <joaofbbg@gmail.com>
2018-03-01 18:47:37 +0000

    add to basket fix

--- src/Web/ViewModels/CatalogItemViewModel.cs
+++ src/Web/ViewModels/CatalogItemViewModel.cs
@@ -3,8 +3,8 @@
 
     public class CatalogItemViewModel
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int CatalogItemId { get; set; }
+        public string CatalogItemName { get; set; }
         public string PictureUri { get; set; }
         public decimal Price { get; set; }
         public string ProductSku { get; set; }
