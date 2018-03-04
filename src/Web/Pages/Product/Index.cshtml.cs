0f190c2875ef25453ab75b86474aa085487908c6
João Gonçalves <joaofbbg@gmail.com>
2018-03-04 14:43:51 +0000

    bootstrap changes and product info changes

--- src/Web/Pages/Product/Index.cshtml.cs
+++ src/Web/Pages/Product/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApplicationCore.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Web.Interfaces;
@@ -12,9 +13,11 @@ namespace Web.Pages.Product
     public class IndexModel : PageModel
     {
         private readonly ICatalogService _catalogService;
-        public IndexModel(ICatalogService catalogService)
+        private readonly IAppLogger<IndexModel> _logger;
+        public IndexModel(ICatalogService catalogService, IAppLogger<IndexModel> logger)
         {
             _catalogService = catalogService;
+            _logger = logger;
         }
         public ProductViewModel ProductModel { get; set; } = new ProductViewModel();
 
@@ -27,9 +30,11 @@ namespace Web.Pages.Product
             //Update default price
             decimal attrDefaultPrice = 0M;
             foreach (var item in ProductModel.Attributes.GroupBy(x => x.AttributeType))
-            {
+            {                
                 attrDefaultPrice += item.First().Attributes.First().Price ?? 0;
             }
+            _logger.LogInformation(">>>>>>> Product Base Price: {0} <<<<<<<<<<<", ProductModel.ProductBasePrice);
+            _logger.LogInformation(">>>>>>> Atribute Default Price: {0} <<<<<<<<<<<", attrDefaultPrice);
             ProductModel.ProductTotalPrice = ProductModel.ProductBasePrice + attrDefaultPrice;
             return Page();
         }
