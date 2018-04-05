f301b2eb95c15f7ef95f8822022ed2376dec3182
João Gonçalves <joaofbbg@gmail.com>
2018-04-06 00:02:12 +0100

    customize categories backend

---
+++ src/ApplicationCore/Specifications/CategorySpecification.cs
@@ -0,0 +1,13 @@
+﻿using ApplicationCore.Entities;
+
+namespace ApplicationCore.Specifications
+{
+    public class CategorySpecification : BaseSpecification<Category>
+    {
+        public CategorySpecification()
+            :base(x => !x.ParentId.HasValue && x.CatalogCategories.Count > 0)
+        {
+            AddInclude(x => x.CatalogCategories);
+        }
+    }
+}
---
+++ src/Web/Services/CustomizeViewModelService.cs
@@ -0,0 +1,35 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using ApplicationCore.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Interfaces;
+using Web.ViewModels;
+
+namespace Web.Services
+{
+    public class CustomizeViewModelService : ICustomizeViewModelService
+    {
+        private readonly IAsyncRepository<Category> _categoryRepository;
+        private readonly ICatalogService _catalogService;
+
+        public CustomizeViewModelService(IAsyncRepository<Category> categoryRepository, ICatalogService catalogService)
+        {
+            _categoryRepository = categoryRepository;
+            _catalogService = catalogService;
+        }
+
+        public async Task<CustomizeViewModel> GetCustomizeItems(int? categoryid)
+        {
+            var categorySpec = new CategorySpecification();
+            var cats = await _categoryRepository.ListAsync(categorySpec);
+
+            return new CustomizeViewModel
+            {
+                Categories = cats.Select(x => (x.Id, x.Name)).ToList()
+            };
+        }
+    }
+}
