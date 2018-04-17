e8be604e01bd12037880134d41b1053db4b583f1
João Gonçalves <joaofbbg@gmail.com>
2018-04-18 00:07:00 +0100

    add customize link to product detail page

---
+++ src/Infrastructure/Data/Migrations/20180417223901_CanCustomize.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class CanCustomize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CanCustomize",
+                table: "Catalog",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CanCustomize",
+                table: "Catalog");
+        }
+    }
+}
