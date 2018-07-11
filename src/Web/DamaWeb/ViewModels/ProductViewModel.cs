384fe01e6bbd49834c1c5e631aebd2d73f1ad272
João Gonçalves <joao.goncalves@x4dev.pt>
2018-07-11 01:50:43 +0100

    lot fixes

--- src/Web/DamaWeb/ViewModels/ProductViewModel.cs
+++ src/Web/DamaWeb/ViewModels/ProductViewModel.cs
@@ -30,7 +30,6 @@ namespace DamaWeb.ViewModels
         public string NameInput { get; set; }
         public int FirstCategoryId { get; set; }
         public IEnumerable<SelectListItem> ProductReferences { get; set; }
-        
     }
 
     public class LinkViewModel
