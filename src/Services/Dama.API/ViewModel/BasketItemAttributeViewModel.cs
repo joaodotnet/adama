8381251ebbf94677dbd25c313de79c5081277b29
João Gonçalves <joaofbbg@gmail.com>
2018-07-11 11:11:18 +0100

    layout changes

--- src/Services/Dama.API/ViewModel/BasketItemAttributeViewModel.cs
+++ src/Services/Dama.API/ViewModel/BasketItemAttributeViewModel.cs
@@ -5,7 +5,7 @@ namespace Dama.API.ViewModels
     public class BasketItemAttributeViewModel
     {
         public int Id { get; set; }
-        public CatalogAttributeType Type { get; set; }
+        public AttributeType Type { get; set; }
         public string Name { get; set; }
         public decimal? Price { get; set; }
     }
