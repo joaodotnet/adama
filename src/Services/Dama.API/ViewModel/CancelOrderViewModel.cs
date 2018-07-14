55675264281a0c398641115fea4a1465ae376de3
João Gonçalves <joao.goncalves@x4dev.pt>
2018-07-14 21:14:50 +0100

    Add cancel order in app

---
+++ src/Services/Dama.API/ViewModel/CancelOrderViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dama.API.ViewModel
+{
+    public class CancelOrderViewModel
+    {
+        public int OrderNumber { get; set; }
+    }
+}
