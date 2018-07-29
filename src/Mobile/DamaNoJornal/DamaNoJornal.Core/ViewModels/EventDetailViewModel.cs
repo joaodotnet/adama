78aa72e9229b023ff0ec4e33c9f98c1e039b4844
João Gonçalves <joaofbbg@gmail.com>
2018-07-29 13:53:54 +0100

    event total by place

---
+++ src/Mobile/DamaNoJornal/DamaNoJornal.Core/ViewModels/EventDetailViewModel.cs
@@ -0,0 +1,81 @@
+﻿using DamaNoJornal.Core.Models.Location;
+using DamaNoJornal.Core.Models.Orders;
+using DamaNoJornal.Core.Services.Order;
+using DamaNoJornal.Core.Services.Settings;
+using DamaNoJornal.Core.ViewModels.Base;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace DamaNoJornal.Core.ViewModels
+{
+    public class EventDetailViewModel : ViewModelBase
+    {
+        private readonly ISettingsService _settingsService;
+        private readonly IOrderService _ordersService;
+
+        private Place _place;
+        private string _title;
+        private string _subTitle;
+
+        public EventDetailViewModel(ISettingsService settingsService, IOrderService ordersService)
+        {
+            _settingsService = settingsService;
+            _ordersService = ordersService;
+        }
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                RaisePropertyChanged(() => Title);
+            }
+        }
+
+        public string SubTitle
+        {
+            get => _subTitle;
+            set
+            {
+                _subTitle = value;
+                RaisePropertyChanged(() => SubTitle);
+            }
+        }
+
+        //public Order Order
+        //{
+        //    get => _order;
+        //    set
+        //    {
+        //        _order = value;
+        //        RaisePropertyChanged(() => Order);
+        //    }
+        //}
+
+        public override async Task InitializeAsync(object navigationData)
+        {
+            if (navigationData is Place)
+            {
+                IsBusy = true;
+
+                var place = navigationData as Place;
+                Title = place.Name;
+
+                // Get order detail info
+                var authToken = _settingsService.AuthAccessToken;
+
+                List<Order> orders = await _ordersService.GetOrderByPlaceAsync(place.Id, authToken);
+                //Total 
+                var total = orders
+                    .SelectMany(x => x.OrderItems)
+                    .Sum(i => i.UnitPrice * i.Quantity);
+                SubTitle = $"Total de Vendas: {total}€";
+                IsBusy = false;
+            }
+        }
+    }
+}
---
+++ src/Mobile/DamaNoJornal/DamaNoJornal.Core/ViewModels/EventViewModel.cs
@@ -0,0 +1,59 @@
+﻿using DamaNoJornal.Core.Helpers;
+using DamaNoJornal.Core.Models.Location;
+using DamaNoJornal.Core.Models.Marketing;
+using DamaNoJornal.Core.Models.User;
+using DamaNoJornal.Core.Services.Marketing;
+using DamaNoJornal.Core.Services.Settings;
+using DamaNoJornal.Core.Services.User;
+using DamaNoJornal.Core.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace DamaNoJornal.Core.ViewModels
+{
+    public class EventViewModel : ViewModelBase
+    {
+        private readonly ISettingsService _settingsService;
+        private readonly IUserService _userService;
+        private Place _place;
+
+        public EventViewModel(ISettingsService settingsService, IUserService userService)
+        {
+            _settingsService = settingsService;
+            _userService = userService;
+        }
+
+        public Place PlaceSelected
+        {
+            get => _place;
+            set
+            {
+                _place = value;
+                RaisePropertyChanged(() => PlaceSelected);
+            }
+        }
+
+        public List<Place> Places => GlobalSetting.Places;
+
+        public ICommand EventDetailCommand => new Command(async() => await EventDetailAsync());
+
+        public override async Task InitializeAsync(object navigationData)
+        {
+            IsBusy = true;
+            PlaceSelected = GlobalSetting.Place2;
+            // Get user
+            //var user = await _userService.GetUserInfoAsync(_settingsService.AuthAccessToken);
+            //PictureUri = Utils.GetLoginPicturiSource(user.Email);
+            //Name = $"Olá {user.Name} {user.LastName}";
+            IsBusy = false;            
+        }
+        private async Task EventDetailAsync()
+        {
+            await NavigationService.NavigateToAsync<EventDetailViewModel>(PlaceSelected);
+        }
+    }
+}
