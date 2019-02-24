bb4df926b0f5599f6cd7d0cc653577d9b9ffc27d
João Gonçalves <joaofbbg@gmail.com>
2019-02-24 22:21:56 +0000

    Add Stock to Grocery

---
+++ src/Libraries/Infrastructure/Data/Grocery/Migrations/20190224215612_StockAdded.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Data.Grocery.Migrations
+{
+    public partial class StockAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Stock",
+                table: "CatalogAttribute",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Stock",
+                table: "Catalog",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Stock",
+                table: "CatalogAttribute");
+
+            migrationBuilder.DropColumn(
+                name: "Stock",
+                table: "Catalog");
+        }
+    }
+}
