cedffc71a941d8dabab93cd647b42acc33f965eb
João Gonçalves <joaofbbg@gmail.com>
2019-03-06 19:04:49 +0000

    add Category Slug on Backoffice

---
+++ src/Libraries/Infrastructure/Data/Migrations/20190306184630_CategorySlug.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class CategorySlug : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "Category",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Category_Slug",
+                table: "Category",
+                column: "Slug",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Category_Slug",
+                table: "Category");
+
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "Category");
+        }
+    }
+}
