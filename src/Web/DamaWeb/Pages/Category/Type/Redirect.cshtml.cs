7067e5e63ae686a7099e83891d56b026ae6b5023
João Gonçalves <joaofbbg@gmail.com>
2019-04-07 15:58:59 +0100

    add redirects do categories

---
+++ src/Web/DamaWeb/Pages/Category/Type/Redirect.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DamaWeb.Pages.Category.Type
+{
+    public class RedirectModel : PageModel
+    {
+        public IActionResult OnGet(string cat, string type, int? p)
+        {
+            return RedirectPermanent(Url.Page("/Category/Type/Index", new { cat, type, p }));
+        }
+    }
+}
