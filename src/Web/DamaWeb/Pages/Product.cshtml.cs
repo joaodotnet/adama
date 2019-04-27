ea8a524d025559674d63d897c1444b1909de030b
João Gonçalves <joaofbbg@gmail.com>
2019-04-27 17:14:18 +0100

    add redirect logic

--- src/Web/DamaWeb/Pages/Product.cshtml.cs
+++ src/Web/DamaWeb/Pages/Product.cshtml.cs
@@ -47,10 +47,17 @@ namespace DamaWeb.Pages
             //Old URL logic
             if (ProductView == null)
             {
-                var catalog = await _catalogService.GetCategoryCatalogItems(id, 0, Constants.ITEMS_PER_PAGE);
-                if (catalog != null && !string.IsNullOrEmpty(catalog.CategoryUrlName))
+                //if category
+                var category = await _catalogService.GetCategoryCatalogItems(id, 0, Constants.ITEMS_PER_PAGE);
+                if (category != null && !string.IsNullOrEmpty(category.CategoryUrlName))
                 {
-                    return RedirectToPagePermanent("/Category/Index", new { id = catalog.CategoryUrlName });
+                    return RedirectToPagePermanent("/Category/Index", new { id = category.CategoryUrlName });
+                }
+                //if product by sku
+                var catalogSlug = await _catalogService.GetSlugFromSkuAsync(id);
+                if(!string.IsNullOrEmpty(catalogSlug))
+                {
+                    return RedirectToPagePermanent("/Product", new { id = catalogSlug });
                 }
                 return NotFound();
             }
