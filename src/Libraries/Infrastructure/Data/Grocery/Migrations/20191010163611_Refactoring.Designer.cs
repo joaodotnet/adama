9df8b979bb4e0f2e43793ab38803926a29c77b0e
João Gonçalves <joaofbbg@gmail.com>
2019-10-10 17:47:49 +0100

    add migrations

--- src/Libraries/Infrastructure/Data/Grocery/Migrations/20191010163611_Refactoring.Designer.cs
+++ src/Libraries/Infrastructure/Data/Grocery/Migrations/20191010163611_Refactoring.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Data.Grocery.Migrations
 {
     [DbContext(typeof(GroceryContext))]
-    partial class GroceryContextModelSnapshot : ModelSnapshot
+    [Migration("20191010163611_Refactoring")]
+    partial class Refactoring
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
