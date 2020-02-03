e5c5af93396fa3c18e3b08a626d840bf8549bc4c
João Gonçalves <joaofbbg@gmail.com>
2020-02-03 12:53:52 +0000

    add conversion tracking; enable hotjar;

--- src/Web/DamaWeb/Pages/Basket/Result.cshtml.cs
+++ src/Web/DamaWeb/Pages/Basket/Result.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApplicationCore.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,10 +10,27 @@ namespace DamaWeb.Pages.Basket
 {
     public class ResultModel : PageModel
     {
+        private readonly IOrderService _orderService;
+
+        public ResultModel(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
         public int OrderNumber { get; set; }
-        public void OnGet(int id)
+        [ViewData]
+        public bool IsAddConversion { get; set; } = false;
+        public async Task OnGetAsync(int id)
         {
             OrderNumber = id;
+
+            //Adwords conversion
+            var order = await _orderService.GetOrderAsync(id);
+
+            if(order?.OrderItems?.Count > 0)
+            {
+                if (order.OrderItems.Any(x => x.ItemOrdered != null && x.ItemOrdered.CatalogItemId == 279))
+                    IsAddConversion = true;
+            }
         }
     }
 }
