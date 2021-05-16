4714a83f6b7e593a70ef01df1477f99bd44b8643
João Gonçalves <joaofbbg@gmail.com>
2021-05-16 19:44:58 +0100

    fix Ef Query

--- src/ApplicationCore/Specifications/CatalogTypeSpecification.cs
+++ src/ApplicationCore/Specifications/CatalogTypeSpecification.cs
@@ -6,7 +6,7 @@ namespace ApplicationCore.Specifications
     public class CatalogTypeSpecification : BaseSpecification<CatalogType>
     {
         public CatalogTypeSpecification(int categoryId)
-            :base(x => x.CatalogItems.Count > 0 && x.Categories.Any(c => c.CategoryId == categoryId))
+            :base(x => x.Categories.Any(c => c.CategoryId == categoryId))
         {
             AddInclude(x => x.CatalogItems);
         }
