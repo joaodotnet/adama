2a393d13a1d2d59fbf48c7787431cd863bd4bc2e
Joao Goncalves <joaofbbg@hotmail.com>
2021-09-25 12:35:02 +0100

    CatalogTypeSpecification: Add filter class WIP

--- src/ApplicationCore/Specifications/CatalogTypeSpecification.cs
+++ src/ApplicationCore/Specifications/CatalogTypeSpecification.cs
@@ -6,35 +6,54 @@ namespace ApplicationCore.Specifications
 {
     public class CatalogTypeSpecification : Specification<CatalogType>
     {
-        public CatalogTypeSpecification(int categoryId)
+        public class CataloTypeFilter
         {
-            Query
+            public int? CategoryId { get; set; }
+            public bool? IncludeHelpers { get; set; }
+            public int? PageIndex { get; set; }
+            public int? PageSize { get; set; }
+            public string Code { get; set; }
+            public string Slug { get; set; }
+        }
+        public CatalogTypeSpecification(CataloTypeFilter filter)
+        {
+            if(filter.CategoryId.HasValue)
+            {
+                Query
                 .Include(x => x.CatalogItems)
-                .Where(x => x.Categories.Any(c => c.CategoryId == categoryId));
-        }
+                .Where(x => x.Categories.Any(c => c.CategoryId == filter.CategoryId));
+            }
 
-        public CatalogTypeSpecification(bool includeHelpers)
-        {
-            if(includeHelpers)
-                Query.Include(x => x.PictureTextHelpers);
-        }
-        
-        public CatalogTypeSpecification(int? pageIndex, int? pageSize)        
-        {
+            if(filter.IncludeHelpers == true)
+            {
+                 Query.Include(x => x.PictureTextHelpers);
+            }
+
+            if(filter.PageIndex.HasValue && filter.PageSize.HasValue)
+            {
+                Query.Skip((filter.PageIndex.Value - 1) * filter.PageSize.Value)
+                    .Take(filter.PageSize.Value);
+
+            }
+
+            if(!string.IsNullOrEmpty(filter.Code))
+            {
+                Query
+                    .Where(x => x.Code.ToUpper() == filter.Code);
+            }
+
+            if(!string.IsNullOrEmpty(filter.Slug))
+            {
+                Query
+                    .Where(x => x.Slug.ToUpper() == filter.Slug);
+            }
+
+            //Todo: Make it optional
             Query
                 .Include(t => t.Categories)
                 .ThenInclude(c => c.Category);
-            
-            if(pageIndex.HasValue && pageSize.HasValue)
-            {
-                Query.Skip((pageIndex.Value - 1) * pageSize.Value)
-                    .Take(pageSize.Value);
-            }
-        }
-        public CatalogTypeSpecification(string code)
-        {
-            Query
-                .Where(x => x.Code.ToUpper() == code);
-        }
+
+
+        }   
     }
 }
